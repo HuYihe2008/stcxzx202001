3930e7c64c4fd948233cabfb4382bf816541315a
HuYihe2008 <2812856215@qq.com>
2021-11-30 22:51:29 +0800

    3.18测试

--- js/header.js
+++ js/header.js
@@ -1,4 +1,39 @@
- $(function(){
+		$(function(){
+        /*公共部分
+         * 导航栏
+         * footer CopyRight
+         */
+        $(".headerpage").load("../../header.html");
+        $("footerpage").load("footer.html");
+    });		$(function(){
+        /*公共部分
+         * 导航栏
+         * footer CopyRight
+         */
+        $(".headerpage").load("../../header.html");
+        $("footerpage").load("footer.html");
+    });		$(function(){
+        /*公共部分
+         * 导航栏
+         * footer CopyRight
+         */
+        $(".headerpage").load("../../header.html");
+        $("footerpage").load("footer.html");
+    });		$(function(){
+        /*公共部分
+         * 导航栏
+         * footer CopyRight
+         */
+        $(".headerpage").load("../../header.html");
+        $("footerpage").load("footer.html");
+    });		$(function(){
+        /*公共部分
+         * 导航栏
+         * footer CopyRight
+         */
+        $(".headerpage").load("../../header.html");
+        $("footerpage").load("footer.html");
+    }); $(function(){
         $(".headerpage").load("../header.html");
 	    $(".header2page").load("../header2.html");
         $(".footerpage").load("../footer.html");
